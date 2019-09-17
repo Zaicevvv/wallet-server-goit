19bd2d044af2c5180342a571cd0eaa9539393042
Zaicev Vladimir <zaicevvv1991@gmail.com>
2019-09-17 21:33:31 +0300

    hw2

--- src/routes/apiRoutes/apiRoutes.js
+++ src/routes/apiRoutes/apiRoutes.js
@@ -1,13 +1,6 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes.js');
-const productsRoutes = require('./productsRoutes');
-const ordersRoutes = require('./ordersRoutes');
+const costsRoutes = require('./costsRoutes.js');
 
-router.use('/user', userRoutes);
-router.use('/products', productsRoutes);
-router.use('/orders', ordersRoutes);
-router.get('/transaction/:date/balance/all', (req, res) => {
-  console.log(req.params);
-})
+router.use('/costs', costsRoutes);
 
 module.exports = router;
